e4dffc806614bcbd624ead01a583fa07931a6702
warlock40 <hacker26warlock40@gmail.com>
2018-07-05 14:57:20 +0530

    comitting one change in new branch
    uploading only one file

---
+++ first.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: warlock
+ * Date: 30-Jun-18
+ * Time: 2:45 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace DefaultNamespace
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Class1
+	{
+		public Class1()
+		{
+			int a=4;
+			int b = 5;
+			int c = a + b;
+			Console.WriteLine("the answer is {0}", c);
+		}
+	}
+}
